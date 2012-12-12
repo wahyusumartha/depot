c6d32d3633b985094e9ccb36c1a8ffbf608ee003
Wahyu Sumartha <wahyusumartha@gmail.com>
2012-12-13 00:26:37 +0800

    running functional testing for CRUD Product Controller

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,6 +3,18 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsum',
+      description: 'Wibbles Are Fun!',
+      image_url: 'lorem.jpg',
+      price: 19.95
+    }
+    @update2 = {
+      title: 'Lorem Ipsum',
+      description: 'Wibbles',
+      image_url: 'lorem.fs',
+      price: -2
+    }
   end
 
   test "should get index" do
@@ -18,7 +30,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +47,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    put :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
